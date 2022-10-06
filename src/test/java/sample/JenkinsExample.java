29cd6ac7974dafb67b3de2bf0c1f6d5638ab82f8
bhargavi-vysyaraju <vysyarajubhargavi@outlook.com>
2022-10-06 17:26:48 +0530

    second commit

--- src/test/java/sample/JenkinsExample.java
+++ src/test/java/sample/JenkinsExample.java
@@ -5,18 +5,25 @@ import org.testng.annotations.Test;
 
 public class JenkinsExample {
 	
-	@Test
+	@Test(priority = 1)
 	public void verifyWithValidCredentials() {
 		System.out.println("First line of test1");
 		Assert.assertTrue(true);
 		System.out.println("Last line of test1");	
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void verifyWithInValidCredentials() {
 		System.out.println("First line of test2");
 		Assert.assertTrue(true);
 		System.out.println("Last line of test2");	
+	}
+	
+	@Test(priority = 3)
+	public void verifyWithNoCredentials() {
+		System.out.println("First line of test3");
+		Assert.assertTrue(false);
+		System.out.println("Last line of test3");	
 	}
 
 }
